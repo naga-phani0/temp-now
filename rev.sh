6513e4cb6cace17d89df155232694e79096e84ec
naga-phani0 <kumar@rivan.in>
2023-01-19 13:50:26 +0530

    revert

---
+++ rev.sh
@@ -0,0 +1,13 @@
+# /bin/bach
+
+git init
+
+git remote add origin https://github.com/naga-phani0/temp-now.git #add file name temp-now
+
+git revert --no-edit HEAD
+
+ git add .
+
+git commit -m 'reverted'
+
+git puch -u origin main
