1d3fc36bc912c09bc3369db42d9df7012d8234f5
naga-phani0 <kumar@rivan.in>
2023-01-23 11:24:04 +0530

    fine monday

--- rev.sh
+++ rev.sh
@@ -5,15 +5,15 @@
 git config --global user.email "kumar@rivan.in"
 git config --global user.name "naga-phani0"
 
-# git remote add origin https://github.com/naga-phani0/temp-now.git #add file name temp-now
+git remote add origin https://github.com/naga-phani0/temp-now.git #add file name temp-now
 
 # git revert --no-edit HEAD
 
 # git reset --hard
-git reset --soft HEAD^
+git reset --hard HEAD^
 
-git add .
+# git add .
 
-git commit -m 'Sucessfully reverted'
+# git commit -m 'Sucessfully reverted'
 
-git push origin main
+# git push origin main
