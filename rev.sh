822ad7e06c3f68d693915dd47bc1ce487477e6bf
naga-phani0 <kumar@rivan.in>
2023-01-23 12:16:31 +0530

    fine monday

--- rev.sh
+++ rev.sh
@@ -10,8 +10,8 @@ git remote add origin https://github.com/naga-phani0/temp-now.git #add file name
 
 # git revert --no-edit HEAD~1
 # git reset --hard HEAD~1 master
-$ git reset --hard HEAD^
-$ git push mathnet -f
+git reset HEAD^ --hard
+git push mathnet -f
 # + git branch refs/heads/main
 # + git config --global user.name 'github-actions[bot]'
 # + git config --global user.email 'github-actions[bot]@users.noreply.github.com'
