53d3760416cb8baf69dd5032c38d25fdef93e1ae
naga-phani0 <kumar@rivan.in>
2023-01-23 11:44:13 +0530

    fine monday

--- rev.sh
+++ rev.sh
@@ -8,9 +8,8 @@ git config --global user.name "naga-phani0"
 # git remote add origin https://github.com/naga-phani0/temp-now.git #add file name temp-now
 
 
-git revert --no-edit HEAD~1
-
-
+# git revert --no-edit HEAD~1
+git reset --hard HEAD~1 master
 
 # git add .
 
